09eef2e2d890975895fdf6d7d102428dfdb7c915
Jimena Garcia <jimena.garcia@cua.uam.mx>
2025-03-12 13:36:26 -0600

    Update descifrar.py

--- descifrar.py
+++ descifrar.py
@@ -1,1 +1,15 @@
 # Plantilla descifrar.py
+
+mensaje = str(input("Dame el mensaje cifrado: "))
+k=int(input("¿Cual fue el desplazamiento?: "))
+
+tamanio= len(mensaje)
+
+descifrado =""
+
+
+for i in  range (0, tamanio):
+
+    descifrado += chr(ord(mensaje[i] ) - k )
+
+print(str(descifrado))
