d95f7c7e612f87481bc6c622fcefce894c9e46a8
Jimena Garcia <jimena.garcia@cua.uam.mx>
2025-03-12 13:32:02 -0600

    Update cifrar.py

--- cifrar.py
+++ cifrar.py
@@ -1,1 +1,20 @@
 # Plantilla cifrar.py
+# Plantilla cifrar.py
+import random
+
+k = random.randint(3,15)
+
+mensaje = str(input("Dame el mensaje a cifrar: "))
+
+tamanio= len(mensaje)
+
+cifrado =""
+
+print("El desplazamiento fue de :",k)
+
+for i in  range (0, tamanio):
+
+    cifrado += chr(ord(mensaje[i] ) + k )
+
+print(str(cifrado))
+
